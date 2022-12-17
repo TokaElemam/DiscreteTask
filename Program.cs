c6ef1b13fd93c714dea60c93c3e03f96d6fd695e
TokaElemam <120840719+TokaElemam@users.noreply.github.com>
2022-12-17 23:58:59 +0200

    Add files via upload
    Project_1

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello at Prime application");
+
+            Console.WriteLine("Please Enter n1");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please Enter n2");
+            int n2 = int.Parse(Console.ReadLine());
+
+
+            for (int i = n1; i <= n2; i++)
+            {
+                int count = 0;
+
+                for (int j = 2; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        count++;
+                        break;
+                    }
+                }
+                if (count == 0 && i != 1)
+                {
+
+                    Console.WriteLine("\n" + i + "is prime");
+
+
+                }
+
+            }
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
